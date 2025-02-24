822a4e79bec1bdddeede944cd0392d41b108ba07
Igor Beschastnov <beschastnovigor@gmail.com>
2025-02-24 17:53:51 +0300

    new toolchain

--- Core/Inc/battery_config.h
+++ Core/Inc/battery_config.h
@@ -1,7 +1,7 @@
-#define undervoltage_lockout_voltage            21.5f   // Volts
-#define undervoltage_lockout_hysteresis         0.8f    // Volts
-#define charged_battery_voltage                 28.8f   // Volts
-#define nominal_charge_current                  4.5f    // Amps
+#define undervoltage_lockout_voltage            30.0f   // Volts
+#define undervoltage_lockout_hysteresis         1.0f    // Volts
+#define charged_battery_voltage                 42.0f   // Volts
+#define nominal_charge_current                  3.0f    // Amps
 
 // user can prioritize discharging of power source 2 or 3. uncomment the desired source (only one!)
 //#define default_prime 0 // the 2 channel
